4914ffc0c5831289a059ffcfaef3e679d4bb58f8
MDen-dev <danilalikan@gmail.com>
2025-07-26 20:49:16 +0500

    Initial commit

---
+++ src/main/java/net/mars/instantwood_mt/InstantWood.java
@@ -0,0 +1,22 @@
+package net.mars.instantwood_mt;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InstantWood implements ModInitializer {
+	public static final String MOD_ID = "instantwood_mt";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModContainer mod = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow();
+		String version = mod.getMetadata().getVersion().getFriendlyString();
+
+		LOGGER.info("[{}] v{} initialized by MARS Team.", MOD_ID, version);
+	}
+}
