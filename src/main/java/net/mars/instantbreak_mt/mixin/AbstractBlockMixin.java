30189ff27785ac74bbe82be3a9941a40b287abd7
MDen-dev <danilalikan@gmail.com>
2025-07-29 12:40:33 +0500

    Lowered the block breaking speed threshold in AbstractBlockMixin from 0.5F to 0.46F, so blocks with hardness >3.5 can break instantly with Efficiency V and Haste II. Version changed to 1.1.0

--- src/main/java/net/mars/instantbreak_mt/mixin/AbstractBlockMixin.java
+++ src/main/java/net/mars/instantbreak_mt/mixin/AbstractBlockMixin.java
@@ -15,7 +15,8 @@ public class AbstractBlockMixin {
 	@Inject(method = "calcBlockBreakingDelta", at = @At("RETURN"), cancellable = true)
 	private void fixSpeed(BlockState state, PlayerEntity player, BlockView world, BlockPos pos, CallbackInfoReturnable<Float> cir) {
 		float result = cir.getReturnValue();
-		if (result > 0.5F & player.getMainHandStack().getMiningSpeedMultiplier(state)>=9) {
+		System.out.println(result);
+		if (result > 0.46F & player.getMainHandStack().getMiningSpeedMultiplier(state)>=9) {
 			cir.setReturnValue(1.0F);
 		}
 	}
