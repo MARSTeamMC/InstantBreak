08dbd67a72a67349bd7ff8be13302508211792a8
MDen <danilalikan@gmail.com>
2026-06-10 00:37:04 +0500

    feat: port to fabrc+1.17.1-1.19.4

--- src/main/java/net/mars/instantbreak_mt/client/BreakDelayOverlay.java
+++ src/main/java/net/mars/instantbreak_mt/client/BreakDelayOverlay.java
@@ -1,38 +1,43 @@
 package net.mars.instantbreak_mt.client;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.mars.instantbreak_mt.InstantBreak;
 import net.mars.instantbreak_mt.InstantBreakClient;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.util.Arm;
 import net.minecraft.util.Identifier;
 
 public class BreakDelayOverlay implements HudRenderCallback {
-    private static final Identifier BREAK_DELAY_ICON = Identifier.of(InstantBreak.MOD_ID, "textures/hud/break_delay.png");
+    private static final Identifier BREAK_DELAY_ICON = new Identifier(InstantBreak.MOD_ID, "textures/hud/break_delay.png");
 
     @Override
-    public void onHudRender(DrawContext drawContext, float v) {
+    public void onHudRender(MatrixStack matrixStack, float v) {
         if (InstantBreakClient.breakDelayEnabled) {
             MinecraftClient client = MinecraftClient.getInstance();
-            PlayerEntity playerEntity = client.player;
-            if (playerEntity != null && !playerEntity.isSpectator() && !client.options.hudHidden) {
-                Arm arm = playerEntity.getMainArm().getOpposite();
-                boolean ItemInOffHand = !playerEntity.getInventory().offHand.getFirst().getItem().equals(Items.AIR);
-                int h = 0;
-                if (ItemInOffHand) {
-                    h = 29;
-                }
-                int i = drawContext.getScaledWindowWidth() / 2;
-                int n = drawContext.getScaledWindowHeight() - 24;
-                int o = i - 91 - 32 - h;
-                if (arm == Arm.RIGHT) {
-                    o = i + 91 + 6 + h;
-                }
+            if (client != null) {
+                PlayerEntity playerEntity = client.player;
+                if (playerEntity != null && !playerEntity.isSpectator() && !client.options.hudHidden) {
+                    Arm arm = playerEntity.getMainArm().getOpposite();
+                    boolean ItemInOffHand = !playerEntity.getInventory().offHand.get(0).getItem().equals(Items.AIR);
+                    int h = 0;
+                    if (ItemInOffHand) {
+                        h = 29;
+                    }
+                    int i = client.getWindow().getScaledWidth() / 2;
+                    int n = client.getWindow().getScaledHeight() - 24;
+                    int o = i - 91 - 32 - h;
+                    if (arm == Arm.RIGHT) {
+                        o = i + 91 + 6 + h;
+                    }
 
-                drawContext.drawTexture(BREAK_DELAY_ICON, o, n, 26, 22, 26, 22, 26, 22);
+                    RenderSystem.setShaderTexture(0, BREAK_DELAY_ICON);
+                    DrawableHelper.drawTexture(matrixStack, o, n, 26, 22, 26, 22, 26, 22);
+                }
             }
         }
     }
