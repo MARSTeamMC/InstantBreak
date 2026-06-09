c6f0fb7856e6c16b03000ab6fd8cdff5791c3393
MDen <danilalikan@gmail.com>
2026-06-10 00:54:08 +0500

    feat: check if client isn't null

--- src/main/java/net/mars/instantbreak_mt/client/BreakDelayOverlay.java
+++ src/main/java/net/mars/instantbreak_mt/client/BreakDelayOverlay.java
@@ -18,22 +18,24 @@ public class BreakDelayOverlay implements HudRenderCallback {
     public void onHudRender(DrawContext drawContext, RenderTickCounter renderTickCounter) {
         if (InstantBreakClient.breakDelayEnabled) {
             MinecraftClient client = MinecraftClient.getInstance();
-            PlayerEntity playerEntity = client.player;
-            if (playerEntity != null && !playerEntity.isSpectator() && !client.options.hudHidden) {
-                Arm arm = playerEntity.getMainArm().getOpposite();
-                boolean ItemInOffHand = !playerEntity.getInventory().offHand.getFirst().getItem().equals(Items.AIR);
-                int h = 0;
-                if (ItemInOffHand) {
-                    h = 29;
-                }
-                int i = drawContext.getScaledWindowWidth() / 2;
-                int n = drawContext.getScaledWindowHeight() - 24;
-                int o = i - 91 - 32 - h;
-                if (arm == Arm.RIGHT) {
-                    o = i + 91 + 6 + h;
-                }
+            if (client != null) {
+                PlayerEntity playerEntity = client.player;
+                if (playerEntity != null && !playerEntity.isSpectator() && !client.options.hudHidden) {
+                    Arm arm = playerEntity.getMainArm().getOpposite();
+                    boolean ItemInOffHand = !playerEntity.getInventory().offHand.getFirst().getItem().equals(Items.AIR);
+                    int h = 0;
+                    if (ItemInOffHand) {
+                        h = 29;
+                    }
+                    int i = drawContext.getScaledWindowWidth() / 2;
+                    int n = drawContext.getScaledWindowHeight() - 24;
+                    int o = i - 91 - 32 - h;
+                    if (arm == Arm.RIGHT) {
+                        o = i + 91 + 6 + h;
+                    }
 
-                drawContext.drawTexture(BREAK_DELAY_ICON, o, n, 26, 22, 26, 22, 26, 22);
+                    drawContext.drawTexture(BREAK_DELAY_ICON, o, n, 26, 22, 26, 22, 26, 22);
+                }
             }
         }
     }
