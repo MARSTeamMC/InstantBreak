359862d25d29dcf2f454ad8cf9d8ba60077208ad
MDen <danilalikan@gmail.com>
2026-06-30 02:33:20 +0500

    chore: clean up project

--- src/main/java/net/mars/instantbreak_mt/event/KeyInputHandler.java
+++ src/main/java/net/mars/instantbreak_mt/event/KeyInputHandler.java
@@ -16,7 +16,6 @@ public class KeyInputHandler {
         ClientTickEvents.END_CLIENT_TICK.register(minecraftClient -> {
             if (breakDelayKey.consumeClick()) {
                 InstantBreakClient.breakDelayEnabled = !InstantBreakClient.breakDelayEnabled;
-                System.out.println(InstantBreakClient.breakDelayEnabled);
             }
         });
     }
