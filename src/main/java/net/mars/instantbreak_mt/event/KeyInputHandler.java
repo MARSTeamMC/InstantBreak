ab03ef94c77591ec601c788f8d94bf97a3cdf376
MDen <danilalikan@gmail.com>
2026-07-03 13:47:58 +0500

    feat: move break delay to its own category

--- src/main/java/net/mars/instantbreak_mt/event/KeyInputHandler.java
+++ src/main/java/net/mars/instantbreak_mt/event/KeyInputHandler.java
@@ -3,12 +3,14 @@ package net.mars.instantbreak_mt.event;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.mars.instantbreak_mt.InstantBreak;
 import net.mars.instantbreak_mt.InstantBreakClient;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyInputHandler {
-    public static final String KEY_ENABLE_BREAK_DELAY = "key.enable_break_delay";
+    public static final String KEY_ENABLE_BREAK_DELAY = "key.instantbreak_mt.enable_break_delay";
 
     public static KeyMapping breakDelayKey;
 
@@ -21,11 +23,13 @@ public class KeyInputHandler {
     }
 
     public static void register() {
+        KeyMapping.Category instantBreakCategory = KeyMapping.Category.register(Identifier.fromNamespaceAndPath(InstantBreak.MOD_ID, "instantbreak"));
+
         breakDelayKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
                 KEY_ENABLE_BREAK_DELAY,
                 InputConstants.Type.KEYSYM,
                 GLFW.GLFW_KEY_R,
-                KeyMapping.Category.GAMEPLAY
+                instantBreakCategory
         ));
 
         registerKeyInputs();
